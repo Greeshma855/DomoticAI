b0a63831cccfd7dea49552274cf112fc4979bd81
krishnamouli8 <moulimorampudi@gmail.com>
2024-11-11 12:04:35 +0530

    Changes in dashboard and functionlity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,61 @@
+// import React from 'react';
+// import { Route, Routes, useNavigate } from "react-router-dom";
+// import Hero from './Hero';
+// import Custom404 from './pages/Custom404';
+// import Pricing from './pages/Pricing';
+// import Signup from './pages/Signup';
+// import Login from './pages/Login';
+// import SmartHomeDashboard from './SmartHomeDashboard';
+// import AnalyticsDashboard from './AnalyticsDashboard';
+// import { useState } from 'react';
+
+// const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+//   const handleLogin = () => {
+//     // Set isAuthenticated to true after a successful login
+//     setIsAuthenticated(true);
+//   };
+
+// const App = () => {
+ 
+//   return (
+//     <Routes>
+//       <Route path="*" element={<Custom404/>} />
+//       <Route path="/" element={<Hero/>} />
+//       <Route path="/pricing" element={<Pricing/>} />
+//       <Route path="/register" element={<Signup/>} />
+//       {/* <Route path="/login" element={<Login/>} /> */}
+//       <Route path="/login" element={<Login onLogin={handleLogin} />} />
+//       {/* <Route path="/dashboard" element={<SmartHomeDashboard/>} /> */}
+//       <Route
+//         path="/dashboard"
+//         element={isAuthenticated ? <SmartHomeDashboard/> : <Navigate to="/login" />}
+//       />
+//       <Route path="/dashboard/analytics" element={<AnalyticsDashboard/>} />
+//     </Routes>
+      
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
+import { useState } from 'react';
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Hero from './Hero';
 import Custom404 from './pages/Custom404';
@@ -7,15 +64,17 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import SmartHomeDashboard from './SmartHomeDashboard';
 import AnalyticsDashboard from './AnalyticsDashboard';
+import { Navigate } from 'react-router-dom';
+// import Dashboard from './Dashboard';
 
-const [isAuthenticated, setIsAuthenticated] = useState(false);
+const App = () => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const handleLogin = () => {
     // Set isAuthenticated to true after a successful login
     setIsAuthenticated(true);
   };
-
-const App = () => {
  
   return (
     <Routes>
@@ -23,9 +82,14 @@ const App = () => {
       <Route path="/" element={<Hero/>} />
       <Route path="/pricing" element={<Pricing/>} />
       <Route path="/register" element={<Signup/>} />
+      {/* <Route path="/dashboardd" element={<Dashboard/>} /> */}
       {/* <Route path="/login" element={<Login/>} /> */}
       <Route path="/login" element={<Login onLogin={handleLogin} />} />
-      <Route path="/dashboard" element={<SmartHomeDashboard/>} />
+      {/* <Route path="/dashboard" element={<SmartHomeDashboard/>} /> */}
+      <Route
+        path="/dashboard"
+        element={isAuthenticated ? <SmartHomeDashboard/> : <Navigate to="/login" />}
+      />
       <Route path="/dashboard/analytics" element={<AnalyticsDashboard/>} />
     </Routes>
       
